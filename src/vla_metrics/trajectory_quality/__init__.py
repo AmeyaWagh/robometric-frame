364e927fe4a40e0b1a53f0fc7bf21c22d042a14a
ameyawagh <ameya555@gmail.com>
2025-11-24 14:15:16 +0530

    Adding ATE and RTE

--- src/vla_metrics/trajectory_quality/__init__.py
+++ src/vla_metrics/trajectory_quality/__init__.py
@@ -4,7 +4,14 @@ This module provides metrics for evaluating the quality of robot trajectories,
 including path length, smoothness, curvature change, and trajectory errors.
 """
 
+from vla_metrics.trajectory_quality.absolute_trajectory_error import AbsoluteTrajectoryError
 from vla_metrics.trajectory_quality.path_length import PathLength
 from vla_metrics.trajectory_quality.path_smoothness import PathSmoothness
+from vla_metrics.trajectory_quality.relative_trajectory_error import RelativeTrajectoryError
 
-__all__ = ["PathLength", "PathSmoothness"]
+__all__ = [
+    "AbsoluteTrajectoryError",
+    "PathLength",
+    "PathSmoothness",
+    "RelativeTrajectoryError",
+]

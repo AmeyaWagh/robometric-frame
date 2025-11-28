b9205288f9949ee59fcfbc8d353fc1db53438937
ameyawagh <ameya555@gmail.com>
2025-11-28 18:02:21 +0530

    added curvature rate metric

--- src/vla_metrics/trajectory_quality/__init__.py
+++ src/vla_metrics/trajectory_quality/__init__.py
@@ -5,12 +5,14 @@ including path length, smoothness, curvature change, and trajectory errors.
 """
 
 from vla_metrics.trajectory_quality.absolute_trajectory_error import AbsoluteTrajectoryError
+from vla_metrics.trajectory_quality.curvature_change import CurvatureChange
 from vla_metrics.trajectory_quality.path_length import PathLength
 from vla_metrics.trajectory_quality.path_smoothness import PathSmoothness
 from vla_metrics.trajectory_quality.relative_trajectory_error import RelativeTrajectoryError
 
 __all__ = [
     "AbsoluteTrajectoryError",
+    "CurvatureChange",
     "PathLength",
     "PathSmoothness",
     "RelativeTrajectoryError",
